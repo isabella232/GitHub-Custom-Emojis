8ecaa8ea9ab5635c5ba9d021290654917a5cb9d3
Rob Garrison <wowmotty@gmail.com>
2018-06-26 18:05:08 -0500

    Meta: Update editor files & add linting

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "rules": {
+    "indent": [0, 2],
+    "quotes": [2, "single", {
+      "allowTemplateLiterals": true,
+      "avoidEscape": true
+    }],
+    "linebreak-style": [2, "unix"],
+    "semi": [2, "always"],
+    "no-console": 0,
+    "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }],
+    "no-empty": 0,
+    "spaced-comment": 2,
+    "arrow-spacing": 2,
+    "no-var": 2,
+    "no-unused-vars": 1
+  },
+  "env": {
+    "es6": true,
+    "browser": true,
+    "greasemonkey": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
+  "extends": "eslint:recommended"
+};
